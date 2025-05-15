56126ece9f668b5f7619ccba264cd57ce6f78513
Gunder Rollufson <gunder.rollufson@gmail.com>
2025-05-15 13:24:42 +0200

    kontaktside init

---
+++ src/app/kontakt/page.tsx
@@ -0,0 +1,12 @@
+export default function Contact() {
+  return (
+    <main className="prose mx-auto p-8">
+      <h1>Kontakt oss</h1>
+      <p>
+        Telefon: <a href="tel:+4799999999">+47 99 99 99 99</a>
+        <br />
+        E-post: <a href="mailto:post@kjolebil.no">post@kjolebil.no</a>
+      </p>
+    </main>
+  );
+}
